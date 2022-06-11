8bcca2cecb64124e2db1a36207a3237abe973fdc
EmmanuelSanchg <emmanuelsanchgdev@gmail.com>
2022-06-11 11:23:54 -0700

    ahora imprime texto en el html sin un alert

--- porciento/app.js
+++ porciento/app.js
@@ -1,21 +1,24 @@
-"use strict"
+"use strict";
 
-function calcularDescuento(precioInicial, descuento){
-        let result = precioInicial * (100 - descuento) / 100
-        return result
+function calcularDescuento(precioInicial, descuento) {
+  let result = (precioInicial * (100 - descuento)) / 100;
+  return result;
 }
 
-
-function realizaAccionUsuario(){
-    let precioInput = document.getElementById("precio")
-    let precioInicial = precioInput.value
-    Number(precioInicial)
-
-    let descuentoInput = document.getElementById("descuento")
-    let descuento = descuentoInput.value
-    Number(descuento)
-    
-    let precioFinal = calcularDescuento(precioInicial, descuento)
-    alert(`el Precio Final a pagar es: ${precioFinal}`)
+function resultCalculo(precioFinal) {
+  let resultPantalla = document.getElementById("precioFinal_");
+  resultPantalla.innerText = `el Precio Final a pagar es: ${precioFinal}`;
 }
 
+function realizaAccionUsuario() {
+  let precioInput = document.getElementById("precio");
+  let precioInicial = precioInput.value;
+  Number(precioInicial);
+
+  let descuentoInput = document.getElementById("descuento");
+  let descuento = descuentoInput.value;
+  Number(descuento);
+
+  let precioFinal = calcularDescuento(precioInicial, descuento);
+  resultCalculo(precioFinal);
+}
