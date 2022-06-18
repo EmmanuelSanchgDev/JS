5dad8bd8d430c1456c12af42ef8358ed9b15fe95
EmmanuelSanchg <emmanuelsanchgdev@gmail.com>
2022-06-17 19:26:57 -0700

    .

---
+++ Junior/1_app.js
@@ -0,0 +1,11 @@
+let num1 = prompt("ingresa el 1er numero")
+let num2 = prompt("ingresa el 2do numero")
+
+
+if(num1 > num2){
+    console.log( `num1: ${num1} es mayor a num2: ${num2}` )
+} else if(num1 < num2){
+    console.log( `num1: ${num1} es menor a num2: ${num2}` )
+} else if(num1 === num2){
+    console.log( `num1: ${num1} es igual a num2: ${num2}` )
+}
